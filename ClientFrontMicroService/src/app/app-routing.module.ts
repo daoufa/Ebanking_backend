07eab9b8a070ef59d8adb76ff67a10ceb3ee85de
Youssef DAFALI <dafaliyoussef01@gmail.com>
2020-05-09 22:43:36 +0000

    components

--- ClientFrontMicroService/src/app/app-routing.module.ts
+++ ClientFrontMicroService/src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { VirementComponent } from "./virement/virement.component";
+import { ComptesComponent } from './comptes/comptes.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RechargeComponent } from './recharge/recharge.component';
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "login", component: LoginComponent },
   { path: "virement", component: VirementComponent },
+  { path: "comptes", component: ComptesComponent },
+  {path: "profiles", component: ProfileComponent },
+  {path: "recharge", component: RechargeComponent },
   { path: "", pathMatch: "full", redirectTo: "/virement" },
 ];
 
