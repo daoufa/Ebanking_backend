9d2c55a9df7fab5cab6825277e707cf496359c4a
ahmed elatrouz <aelatrouz@gmail.com>
2020-05-10 01:08:35 +0000

    recharge component

---
+++ ClientFrontMicroService/src/app/model/reacharge.model.ts
@@ -0,0 +1,10 @@
+import {CompteEpargne} from "./compteEpargne.model";
+
+export class Recharge {
+
+  public id: number;
+  public numero = 0o632302566;
+  public montant = 20;
+  public date = '10/02/2020';
+
+}
