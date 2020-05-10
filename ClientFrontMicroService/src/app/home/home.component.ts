04e7a6c0e2eb8f3eb262b6fa7fd91950219bcdbf
ahmed elatrouz <aelatrouz@gmail.com>
2020-05-10 17:21:57 +0000

    nothing

--- ClientFrontMicroService/src/app/home/home.component.ts
+++ ClientFrontMicroService/src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
   }
 
   getComptes() {
-    this.comptesService.getComptes(this.numclient).subscribe(
+    this.comptesService.getComptesByClientId(this.numclient).subscribe(
       (data) => {
         this.comptes = data;
       },
